474bd88b4e13520996084e322d484e2a4532d3d6
Umer-Islam <139012668+Umer-Islam@users.noreply.github.com>
2024-02-07 16:18:10 +0500

    create dynamic grid

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
- 
+let square = document.querySelector(".grid");
+document.addEventListener("DOMContentLoaded", function () {
+  createBoard(44);
+});
+
+function createBoard(size) {
+  square.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  square.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+}
